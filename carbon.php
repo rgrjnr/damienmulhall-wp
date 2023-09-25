dcedf6fa2f6c49e736c68b67d655b55d119edeac
Roger Junior <roger@rogerjunior.com>
2023-09-25 19:39:09 +0100

    update

---
+++ carbon.php
@@ -0,0 +1,18 @@
+<?php
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action('carbon_fields_register_fields', 'rgrjnr_attach_theme_options');
+
+function rgrjnr_attach_theme_options()
+{
+}
+
+add_action('after_setup_theme', 'rgrjnr_crb_load');
+
+function rgrjnr_crb_load()
+{
+    require_once('vendor/autoload.php');
+    \Carbon_Fields\Carbon_Fields::boot();
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4e796dd78d2f33823c68ae179505fd89e469654a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4e796dd78d2f33823c68ae179505fd89e469654a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'htmlburger/carbon-fields' => array(
+            'pretty_version' => 'v3.6.0',
+            'version' => '3.6.0.0',
+            'reference' => '47f1538cca6cd2860d1b37a040a42c59eb798d76',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
